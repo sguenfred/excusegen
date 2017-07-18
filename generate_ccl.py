a167a0c2f60f7fc62cb3ef393fccf5dc0f00adc6
Guillaume MOTTE <gmotte.ext@orange.com>
2017-07-18 12:12:51 +0200

    Nouvelle version

--- generate_ccl.py
+++ generate_ccl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import random, ConfigParser
-conf_file="./generate_utf8.conf"
+conf_file="./generate.conf"
 conf_vars=dict()
 
 configParser = ConfigParser.RawConfigParser()
@@ -15,29 +15,46 @@ for element in conf_list:
 if "prelude" in conf_vars:
     string=conf_vars["prelude"]
     string+="\n"
-if "first_fixed" in conf_vars:
-    string+=conf_vars["first_fixed"]
-if "first_var" in conf_vars:
-    string+=conf_vars["first_var"][random.randint(0,len(conf_vars["first_var"])-1)]
-    string+=" "
-if "second_var" in conf_vars:
-    string+=conf_vars["second_var"][random.randint(0,len(conf_vars["second_var"])-1)]
-if "second_fixed" in conf_vars:
-    string+=conf_vars["second_fixed"]
-if "third_var" in conf_vars:
-    string+=conf_vars["third_var"][random.randint(0,len(conf_vars["third_var"])-1)]
-if "third_fixed" in conf_vars:
-    string+=conf_vars["third_fixed"]
-if "fourth_var" in conf_vars:
-    string+=conf_vars["fourth_var"][random.randint(0,len(conf_vars["fourth_var"])-1)]
-    string+=" "
-if "fifth_var" in conf_vars:
-    string+=conf_vars["fifth_var"][random.randint(0,len(conf_vars["fifth_var"])-1)]
-if "fouth_fixed" in conf_vars:
-    string+=conf_vars["fouth_fixed"]
-if "sixth_var" in conf_vars:
-    string+=conf_vars["sixth_var"][random.randint(0,len(conf_vars["sixth_var"])-1)]
-    string+="\n"
+if "first" in conf_vars:
+    string+=conf_vars["first"]
+
+if random.randint(0,1) == 0:
+    if "entity" in conf_vars:
+        string+=conf_vars["entity"][random.randint(0,len(conf_vars["entity"])-1)]
+        string+=" "
+    if "entity_fail" in conf_vars:
+        string+=conf_vars["entity_fail"][random.randint(0,len(conf_vars["entity_fail"])-1)]
+        string+=" "
+else:
+    if "person" in conf_vars:
+        string+=conf_vars["person"][random.randint(0,len(conf_vars["person"])-1)]
+        string+=" "
+    if "person_fail" in conf_vars:
+        string+=conf_vars["person_fail"][random.randint(0,len(conf_vars["person_fail"])-1)]
+        string+=" "
+
+if "second" in conf_vars:
+    string+=conf_vars["second"]
+
+rand = random.randint(0,1)
+if rand == 0:
+    if "entity" in conf_vars:
+        string+=conf_vars["entity"][random.randint(0,len(conf_vars["entity"])-1)]
+        string+=" "
+else:
+    if "person" in conf_vars:
+        string+=conf_vars["person"][random.randint(0,len(conf_vars["person"])-1)]
+        string+=" "
+
+if rand == 0:
+    if "entity_fail" in conf_vars:
+        string+=conf_vars["entity_fail"][random.randint(0,len(conf_vars["entity_fail"])-1)]
+        string+=" "
+else:
+    if "person_fail" in conf_vars:
+        string+=conf_vars["person_fail"][random.randint(0,len(conf_vars["person_fail"])-1)]
+        string+=" "
+
 if "finish" in conf_vars:
     string+=conf_vars["finish"]
 
